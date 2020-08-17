92c3656be7f04bee7c735b4ed7ace1e56706d79d
HarisAhmad43 <haris18896@gmail.com>
2020-08-17 12:12:52 -0700

    almost set

--- eWallet/wallet/urls.py
+++ eWallet/wallet/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('dashboard/', views.dashboard, name="dashboard"),
 
+    path('register/', views.register, name="register"),
+    path('login/', views.login, name="login")
+
 
 
 ]
--- eWallet/wallet/views.py
+++ eWallet/wallet/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def register(request):
+    context = {}
+    return render(request, 'wallet/register.html', context)
+
+def login(request):
+    context = {}
+    return render(request, 'wallet/login.html', context)
+
+
 def home(request):
     context = {}
     return render(request, 'wallet/main.html', context)
